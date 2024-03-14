4cfcb284736d2981860e3145141c6798d25c1aff
Máté Csörnyei <mate@csornyei.com>
2024-03-14 10:29:53 +0100

    Simple rust tcp server

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_client(stream);
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    for line in buf_reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
+
+    println!("Connection closed");
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
